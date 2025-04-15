fd70b4acc82a8014d9dafd140e54731f26743c55
Bismark <bettkipkoech45@gmail.com>
2025-04-16 02:21:22 +0300

    search bar

--- expense-tracker/src/App.jsx
+++ expense-tracker/src/App.jsx
@@ -1,34 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState } from 'react'
+import ExpenseForm from './Components/ExpenseForm'
+import ExpenseTable from './Components/ExpenseTable'
+import SearchBar from './Components/SearchBar'
 import './App.css'
 
+<img src="/vite.svg" className="logo react" alt="logo" />
+
+
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [expenses, setExpenses] = useState([])
+  const [searchTerm, setSearchTerm] = useState('')
+  const [sortKey, setSortKey] = useState(null)
+
+  const handleAddExpense = (newExpense) => {
+    setExpenses([...expenses, newExpense])
+  }
+
+  const handleSearch = (term) => {
+    setSearchTerm(term.toLowerCase())
+  }
+
+  const handleDelete = (id) => {
+    setExpenses(expenses.filter((expense) => expense.id !== id))
+  }
+
+  const handleSort = (key) => {
+    setSortKey(key)
+  }
+
+  let filteredExpenses = expenses.filter(expense =>
+    expense.name.toLowerCase().includes(searchTerm) ||
+    expense.description.toLowerCase().includes(searchTerm) ||
+    expense.category.toLowerCase().includes(searchTerm)
+  )
+
+  if (sortKey) {
+    filteredExpenses.sort((a, b) =>
+      a[sortKey].localeCompare(b[sortKey])
+    )
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="container">
+      <h1>Expense Tracker</h1>
+      <h3> Start taking control of your finances and life. Record, categorize and Analyze your spending.</h3>
+      
+      <ExpenseForm onAddExpense={handleAddExpense} />
+      <SearchBar onSearch={handleSearch} />
+      <ExpenseTable
+        expenses={filteredExpenses}
+        onDelete={handleDelete}
+        onSort={handleSort}
+      />
+    </div>
   )
 }
 
